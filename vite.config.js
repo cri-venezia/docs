377141e39b68ea1f7580e0b2f7171c1ca5097c12
Artek <darkartek@gmail.com>
2025-10-24 06:52:38 +0200

    fix(ci): vite config

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from 'tailwindcss'
+import path from 'path' // <-- Importa il modulo 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  
-  // Determina la base URL
-  // 1. In produzione (build), usa la variabile d'ambiente GITHUB_REPOSITORY_NAME
-  // 2. In sviluppo (dev), usa la root '/'
-  const base = mode === 'production' && process.env.GITHUB_REPOSITORY_NAME
-    ? `/${process.env.GITHUB_REPOSITORY_NAME}/`
-    : '/'
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const repoName = env.GITHUB_REPOSITORY ? env.GITHUB_REPOSITORY.split('/')[1] : '/'
 
   return {
-    plugins: [
-      vue(),
-    ],
-    css: {
-      postcss: {
-        plugins: [tailwindcss()],
+    plugins: [vue()],
+    base: mode === 'production' ? `/${repoName}/` : '/',
+    
+    // --- AGGIUNTA ---
+    // Aggiungi questo blocco per dire a Vite
+    // che '@' significa './src'
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
     },
-    base: base, // Imposta la base URL dinamicamente
+    // --- FINE AGGIUNTA ---
   }
 })
